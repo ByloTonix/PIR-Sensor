7afda92ed4b4b9b8acc4afa5b1e1a44ab60f9641
MatroCholo <64357478+MatroCholo@users.noreply.github.com>
2022-11-13 19:29:57 +0300

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,52 +1,31 @@
 import sys
-import time
+from time import sleep, time
 import RPi.GPIO as gpio
-import subprocess
+from subprocess import call
  
 gpio.setmode(gpio.BOARD)
-poweroff_delay = 10
-check_delay = 0.1
+poweroff_delay = 60
+check_delay = 0.85
 pir_pin = 11
  
 def main():
     gpio.setup(pir_pin, gpio.IN)
     isDisabled = False
-    last_motion_time = time.time()
+    last_motion_time = time()
  
     while True:
-        if gpio.input(pir_pin) == 1:
-            # print('Датчик зафиксировал движение...')
-            last_motion_time = time.time()
+        if gpio.input(pir_pin):
+            last_motion_time = time()
             sys.stdout.flush()
-            if isDisabled == True:
-                # print('Попытка включения экрана...')
+            if isDisabled:
                 isDisabled = False
-                turn_on()
-            else:
-                # print('Экран был включен, ничего не меняю')
-                pass
+                call('sudo vcgencmd display_power 1', shell=True)
         else:
-            if time.time() > (last_motion_time + poweroff_delay):
-                # print('Наступил момент выключения экрана. Попытка выключения...')
-                if isDisabled == False:
+            if time() > (last_motion_time + poweroff_delay) and not isDisabled:
                     isDisabled = True
-                    turn_off()
-                else:
-                    # print('Экран был выключен, ничего не меняю')
-                    pass
-            else:
-                # print('Время ещё не настало, осталось ', (last_motion_time + poweroff_delay) - time.time())
-                pass
-        time.sleep(check_delay)
- 
-def turn_on():
-    subprocess.call('sudo vcgencmd display_power 1', shell=True)
-    # print('Экран включен')
-    
-def turn_off():
-    subprocess.call('sudo vcgencmd display_power 0', shell=True)
-    # print('Экран выключен')
+                    call('sudo vcgencmd display_power 0', shell=True)
+        sleep(check_delay)
  
 if __name__ == '__main__':
-    turn_on()
+    subprocess.call('sudo vcgencmd display_power 1', shell=True)
     main()
